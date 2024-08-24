456c347d321421ff16846b4c3dd8d448f583bae9
SatkarNiraula <satkarniraula69@gmail.com>
2024-08-24 13:33:20 +0545

    update :: all remaining endpoints configured

--- lib/config.dart
+++ lib/config.dart
@@ -15,15 +15,28 @@ class Config {
   // ===================== Auth Routes ===========================a
   static const String login = "auth/login";
   static const String userProfile = "user/profile";
+
   static const String getPersonalDetails =
       "userdetail/userdetails-by-currentuser";
   static const String createPersonalDetail = 'userdetail/create-userdetail';
   static const String personalDetailByID =
       'userdetail/'; // RUD--> read, update, delete
+
   static const String createEducation = 'education/create-edu';
   static const String educationByPersonalDetail =
       "education/edu-by-userdetail/";
+
   static const String createExperience = 'experience/create-exp';
   static const String experienceByPersonalDetail =
       'experience/exp-by-userdetail/';
+
+  static const String createProject = 'projects/create-proj';
+  static const String projectByPersonalDetail = 'projects/proj-by-userdetail/';
+
+  static const String createSkill = 'skills/create-skills';
+  static const String skillByPersonalDetail = 'skills/skill-by-userdetail/';
+
+  static const String createObjective = 'objective/create-obj';
+  static const String objectiveByPersonalDetail =
+      'objective/obj-by-userdetail/';
 }
